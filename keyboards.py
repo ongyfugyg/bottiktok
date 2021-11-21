a311414a26a2cef5e0cc8d5c3771bf6b54afbe81
ongyfugyg <94737880+ongyfugyg@users.noreply.github.com>
2021-11-21 21:06:59 +0200

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -12,3 +12,17 @@ def back_kb():
 	back_kb1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 	back_kb1.add(button1)
 	return back_kb1
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+def admin_menu():
+	button2 = KeyboardButton('Рассылка')
+	button3 = KeyboardButton('Назад')
+	admin1_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+	admin1_kb.add(button2)
+	admin1_kb.add(button3)
+	return admin1_kb
+def back_2():
+	back_kb1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+	button = KeyboardButton('Отмена')
+	back_kb1.add(button)
+	return back_kb1
